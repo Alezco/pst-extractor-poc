2002f8787b92d97d37a0f9450daef013205c5cdf
Alezco <benjaminmorali4@hotmail.fr>
2021-02-08 18:15:45 +0100

    add command line arguments

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const {PSTFile} = require('pst-extractor');
 const {printPstTree} = require('./print-pst-tree');
 const {printPstAppointments} = require('./print-pst-calendar');
 
-const pstFile = new PSTFile(resolve('./appointments.pst'));
-printPstTree(pstFile.getRootFolder());
+const filePath = process.argv.length === 3
+    ? process.argv[process.argv.length - 1]
+    : './appointments.pst';
 
+const pstFile = new PSTFile(resolve(filePath));
+printPstTree(pstFile.getRootFolder());
 /*const appointmentsPstFile = new PSTFile(resolve('./appointments.pst'));
 printPstAppointments(appointmentsPstFile);*/
